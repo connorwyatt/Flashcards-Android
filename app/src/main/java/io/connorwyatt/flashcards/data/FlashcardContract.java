c9c7f748b7deb63973151ed7bd8ef6d65c430ad0
Connor Wyatt <connorwyatt1@gmail.com>
2016-11-10 21:44:39 +0000

    Flashcard DB entries now store timestamps for last updated on and created on

--- app/src/main/java/io/connorwyatt/flashcards/data/FlashcardContract.java
+++ app/src/main/java/io/connorwyatt/flashcards/data/FlashcardContract.java
@@ -1,13 +1,13 @@
 package io.connorwyatt.flashcards.data;
 
-import android.provider.BaseColumns;
-
 public final class FlashcardContract {
     public static final String TABLE_NAME = "flashcard";
 
     public static final String TABLE_CREATE = "create table "
             + TABLE_NAME + "( "
             + Columns._ID + " integer primary key autoincrement, "
+            + Columns._CREATED_ON + " int not null, "
+            + Columns._LAST_MODIFIED_ON + " int not null, "
             + Columns.TITLE + " text not null, "
             + Columns.TEXT + " text not null"
             + ");";
@@ -15,7 +15,7 @@ public final class FlashcardContract {
     private FlashcardContract() {
     }
 
-    public static class Columns implements BaseColumns {
+    public static class Columns implements BaseColumnsTimeline {
         public static final String TITLE = "title";
         public static final String TEXT = "text";
     }
