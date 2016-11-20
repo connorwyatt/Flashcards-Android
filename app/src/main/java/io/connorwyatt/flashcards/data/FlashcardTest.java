e11c1c8616af78b0fbf6d9e539ad859db6dd0630
Connor Wyatt <connorwyatt1@gmail.com>
2016-11-20 12:06:52 +0000

    Improved FlashcardTest entity by using an enum for the rating and "boxed" primitives so that null values can be set

--- app/src/main/java/io/connorwyatt/flashcards/data/FlashcardTest.java
+++ app/src/main/java/io/connorwyatt/flashcards/data/FlashcardTest.java
@@ -1,25 +1,43 @@
 package io.connorwyatt.flashcards.data;
 
 public class FlashcardTest extends BaseEntity {
-    private float rating;
-    private long flashcardId;
+    private Rating rating;
+    private Long flashcardId;
 
     public FlashcardTest() {
     }
 
-    public float getRating() {
+    public Rating getRating() {
         return rating;
     }
 
-    public void setRating(float rating) {
+    public void setRating(Rating rating) {
         this.rating = rating;
     }
 
-    public long getFlashcardId() {
+    public void setRatingPositive() {
+        this.setRating(Rating.POSITIVE);
+    }
+
+    public void setRatingNeutral() {
+        this.setRating(Rating.NEUTRAL);
+    }
+
+    public void setRatingNegative() {
+        this.setRating(Rating.NEGATIVE);
+    }
+
+    public Long getFlashcardId() {
         return flashcardId;
     }
 
-    public void setFlashcardId(long flashcardId) {
+    public void setFlashcardId(Long flashcardId) {
         this.flashcardId = flashcardId;
+    }
+
+    public static enum Rating {
+        POSITIVE,
+        NEUTRAL,
+        NEGATIVE
     }
 }
