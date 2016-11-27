c7bd349fa0354ba9c4f051a6569f97fe964d9b7e
Connor Wyatt <connorwyatt1@gmail.com>
2016-11-27 21:30:34 +0000

    Test card front is now styled as per designs

--- app/src/main/java/io/connorwyatt/flashcards/FlashcardTestCardFragment.java
+++ app/src/main/java/io/connorwyatt/flashcards/FlashcardTestCardFragment.java
@@ -6,81 +6,28 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageButton;
 import android.widget.TextView;
-import io.connorwyatt.flashcards.data.entities.FlashcardTest;
-import io.connorwyatt.flashcards.data.datasources.FlashcardTestDataSource;
 
-import java.util.ArrayList;
-import java.util.List;
+import io.connorwyatt.flashcards.data.entities.FlashcardTest;
 
 public class FlashcardTestCardFragment extends Fragment {
-    private List<ImageButton> buttons = new ArrayList<>();
     private FlashcardTest flashcardTest = new FlashcardTest();
-    private ImageButton currentlySelectedButton;
 
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        ViewGroup viewGroup = (ViewGroup) inflater.inflate(R.layout.flashcard_test_card, container, false);
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable
+            Bundle savedInstanceState) {
+        ViewGroup viewGroup = (ViewGroup) inflater.inflate(R.layout.flashcard_test_card,
+                container, false);
 
         TextView title = (TextView) viewGroup.findViewById(R.id.flashcard_test_card_title);
-        TextView text = (TextView) viewGroup.findViewById(R.id.flashcard_test_card_text);
-
-        buttons.add((ImageButton) viewGroup.findViewById(R.id.flashcard_test_card_negative_button));
-        buttons.add((ImageButton) viewGroup.findViewById(R.id.flashcard_test_card_neutral_button));
-        buttons.add((ImageButton) viewGroup.findViewById(R.id.flashcard_test_card_positive_button));
-
-        setButtonClickHandlers();
 
         Bundle arguments = getArguments();
 
         flashcardTest.setFlashcardId(arguments.getLong(ARGUMENT_KEYS.ID));
         title.setText(arguments.getString(ARGUMENT_KEYS.TITLE));
-        text.setText(arguments.getString(ARGUMENT_KEYS.TEXT));
 
         return viewGroup;
-    }
-
-    private void setButtonClickHandlers() {
-        for (final ImageButton currentButton : buttons) {
-            currentButton.setImageAlpha(100);
-            currentButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    onButtonClick(currentButton);
-                }
-            });
-        }
-    }
-
-    private void onButtonClick(ImageButton button) {
-        int buttonId = button.getId();
-
-        switch (buttonId) {
-            case R.id.flashcard_test_card_positive_button:
-                flashcardTest.setRatingPositive();
-                break;
-            case R.id.flashcard_test_card_neutral_button:
-                flashcardTest.setRatingNeutral();
-                break;
-            case R.id.flashcard_test_card_negative_button:
-                flashcardTest.setRatingNegative();
-                break;
-        }
-
-        if (currentlySelectedButton != null) {
-            currentlySelectedButton.setImageAlpha(100);
-        }
-
-        currentlySelectedButton = button;
-
-        currentlySelectedButton.setImageAlpha(255);
-
-        FlashcardTestDataSource ftds = new FlashcardTestDataSource(getActivity());
-        ftds.open();
-        flashcardTest = ftds.save(flashcardTest);
-        ftds.close();
     }
 
     public static class ARGUMENT_KEYS {
