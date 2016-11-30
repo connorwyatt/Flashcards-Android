102b249303d64012b6904486db559307f91888d9
Connor Wyatt <connorwyatt1@gmail.com>
2016-11-30 11:02:52 +0000

    Added ability to switch between the front and the back of a card in test mode

--- app/src/main/java/io/connorwyatt/flashcards/FlashcardTestCardFragment.java
+++ app/src/main/java/io/connorwyatt/flashcards/FlashcardTestCardFragment.java
@@ -1,38 +1,97 @@
 package io.connorwyatt.flashcards;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
-import io.connorwyatt.flashcards.data.entities.FlashcardTest;
-
 public class FlashcardTestCardFragment extends Fragment {
-    private FlashcardTest flashcardTest = new FlashcardTest();
+    private boolean isFlipped = false;
 
-    @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable
-            Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
         ViewGroup viewGroup = (ViewGroup) inflater.inflate(R.layout.flashcard_test_card,
                 container, false);
 
-        TextView title = (TextView) viewGroup.findViewById(R.id.flashcard_test_card_title);
-
-        Bundle arguments = getArguments();
-
-        flashcardTest.setFlashcardId(arguments.getLong(ARGUMENT_KEYS.ID));
-        title.setText(arguments.getString(ARGUMENT_KEYS.TITLE));
+        getChildFragmentManager()
+                .beginTransaction()
+                .add(R.id.flashcard_test_card_frame, new CardFrontFragment())
+                .commit();
 
         return viewGroup;
+    }
+
+    private void flipCard() {
+        if (!isFlipped) {
+            getChildFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.flashcard_test_card_frame, new CardBackFragment())
+                    .commit();
+
+            isFlipped = !isFlipped;
+        }
     }
 
     public static class ARGUMENT_KEYS {
         public static String ID = "ID";
         public static String TITLE = "TITLE";
         public static String TEXT = "TEXT";
+    }
+
+    public static class CardFrontFragment extends Fragment {
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            CardView card = (CardView) inflater.inflate(R.layout.flashcard_test_card_front,
+                    container, false);
+
+            final FlashcardTestCardFragment parentFragment = (FlashcardTestCardFragment)
+                    getParentFragment();
+
+            Bundle arguments = parentFragment.getArguments();
+            String titleText = arguments.getString(FlashcardTestCardFragment.ARGUMENT_KEYS.TITLE);
+
+            TextView title = (TextView) card.findViewById(R.id.flashcard_test_card_title);
+            title.setText(titleText);
+
+            ImageButton flipButton = (ImageButton) card.findViewById(R.id
+                    .flashcard_test_card_flip_button);
+            flipButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    parentFragment.flipCard();
+                }
+            });
+
+            return card;
+        }
+    }
+
+    public static class CardBackFragment extends Fragment {
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            CardView card = (CardView) inflater.inflate(R.layout.flashcard_test_card_back,
+                    container, false);
+
+            final FlashcardTestCardFragment parentFragment = (FlashcardTestCardFragment)
+                    getParentFragment();
+
+            Bundle arguments = parentFragment.getArguments();
+            String titleText = arguments.getString(FlashcardTestCardFragment.ARGUMENT_KEYS.TITLE);
+            String textText = arguments.getString(FlashcardTestCardFragment.ARGUMENT_KEYS.TEXT);
+
+            TextView title = (TextView) card.findViewById(R.id.flashcard_test_card_title);
+            title.setText(titleText);
+            TextView text = (TextView) card.findViewById(R.id.flashcard_test_card_text);
+            text.setText(textText);
+
+            return card;
+        }
     }
 }
