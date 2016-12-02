ad52759767f56c3758a1b231d46b555cd20ae9e4
Connor Wyatt <connorwyatt1@gmail.com>
2016-12-02 23:47:11 +0000

    Added ability to save rate confidence with flashcards in test mode

--- app/src/main/java/io/connorwyatt/flashcards/FlashcardTestCardFragment.java
+++ app/src/main/java/io/connorwyatt/flashcards/FlashcardTestCardFragment.java
@@ -9,6 +9,12 @@ import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import io.connorwyatt.flashcards.data.datasources.FlashcardTestDataSource;
+import io.connorwyatt.flashcards.data.entities.FlashcardTest;
+
 public class FlashcardTestCardFragment extends Fragment {
     private boolean isFlipped = false;
 
@@ -75,6 +81,22 @@ public class FlashcardTestCardFragment extends Fragment {
     }
 
     public static class CardBackFragment extends Fragment {
+        FlashcardTest flashcardTest = new FlashcardTest();
+        List<ImageButton> buttons = new ArrayList<>();
+        ImageButton currentlySelectedButton;
+
+        private void setButtonClickHandlers() {
+            for (final ImageButton currentButton : buttons) {
+                currentButton.setImageAlpha(100);
+                currentButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        onButtonClick(currentButton);
+                    }
+                });
+            }
+        }
+
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
@@ -88,12 +110,56 @@ public class FlashcardTestCardFragment extends Fragment {
             String titleText = arguments.getString(FlashcardTestCardFragment.ARGUMENT_KEYS.TITLE);
             String textText = arguments.getString(FlashcardTestCardFragment.ARGUMENT_KEYS.TEXT);
 
+            flashcardTest.setFlashcardId(arguments.getLong(FlashcardTestCardFragment
+                    .ARGUMENT_KEYS.ID));
+
             TextView title = (TextView) card.findViewById(R.id.flashcard_test_card_title);
             title.setText(titleText);
             TextView text = (TextView) card.findViewById(R.id.flashcard_test_card_text);
             text.setText(textText);
 
+            buttons.add((ImageButton) card.findViewById(R.id.flashcard_test_card_negative_button));
+            buttons.add((ImageButton) card.findViewById(R.id.flashcard_test_card_neutral_button));
+            buttons.add((ImageButton) card.findViewById(R.id.flashcard_test_card_positive_button));
+
+            setButtonClickHandlers();
+
             return card;
         }
+
+        private void onButtonClick(ImageButton button) {
+            int buttonId = button.getId();
+
+            switch (buttonId) {
+                case R.id.flashcard_test_card_positive_button:
+                    flashcardTest.setRatingPositive();
+                    break;
+                case R.id.flashcard_test_card_neutral_button:
+                    flashcardTest.setRatingNeutral();
+                    break;
+                case R.id.flashcard_test_card_negative_button:
+                    flashcardTest.setRatingNegative();
+                    break;
+            }
+
+            saveFlashcardTest();
+
+            if (currentlySelectedButton != null) {
+                currentlySelectedButton.setImageAlpha(100);
+            }
+
+            currentlySelectedButton = button;
+
+            currentlySelectedButton.setImageAlpha(255);
+        }
+
+        private void saveFlashcardTest() {
+            FlashcardTestDataSource ftds = new FlashcardTestDataSource(getActivity());
+            ftds.open();
+            flashcardTest = ftds.save(flashcardTest);
+            ftds.close();
+        }
+
+
     }
 }
