e8eee78d9ab0941a84565558ecbec8e247d09b6a
Connor Wyatt <connorwyatt1@gmail.com>
2016-12-03 17:56:02 +0000

    FlashcardTest entities are stored in the activity to allow them to be accessed from the summary. Also added PerformanceBreakdown interface for getting the statistics for the summary.

--- app/src/main/java/io/connorwyatt/flashcards/activities/FlashcardTestActivity.java
+++ app/src/main/java/io/connorwyatt/flashcards/activities/FlashcardTestActivity.java
@@ -8,15 +8,25 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
 
+import com.android.internal.util.Predicate;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import io.connorwyatt.flashcards.R;
 import io.connorwyatt.flashcards.adapters.FlashcardTestPagerAdapter;
 import io.connorwyatt.flashcards.data.datasources.FlashcardDataSource;
 import io.connorwyatt.flashcards.data.entities.Flashcard;
+import io.connorwyatt.flashcards.data.entities.FlashcardTest;
+import io.connorwyatt.flashcards.utils.ListUtils;
 import io.connorwyatt.flashcards.views.directionalviewpager.DirectionalViewPager;
 
 public class FlashcardTestActivity extends AppCompatActivity {
+    private int initialCount;
+    private PerformanceBreakdown performanceBreakdown = createPerformanceBreakdown();
+    private HashMap<Long, FlashcardTest> flashcardTestMap = new HashMap<>();
+
     @Override
     public void onBackPressed() {
         new AlertDialog.Builder(this)
@@ -63,6 +73,10 @@ public class FlashcardTestActivity extends AppCompatActivity {
         context.startActivity(intent);
     }
 
+    public void updateFlashcardTest(FlashcardTest flashcardTest) {
+        flashcardTestMap.put(flashcardTest.getId(), flashcardTest);
+    }
+
     private void setUpViewPager() {
         List<Flashcard> flashcards;
         Intent intent = getIntent();
@@ -75,6 +89,8 @@ public class FlashcardTestActivity extends AppCompatActivity {
             flashcards = fds.getAll();
         }
         fds.close();
+
+        initialCount = flashcards.size();
 
         FlashcardTestPagerAdapter adapter = new FlashcardTestPagerAdapter(
                 getFragmentManager(), flashcards);
@@ -92,6 +108,74 @@ public class FlashcardTestActivity extends AppCompatActivity {
                 Toast.makeText(FlashcardTestActivity.this, skipMessage, Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    private PerformanceBreakdown createPerformanceBreakdown() {
+        return new PerformanceBreakdown() {
+            @Override
+            public int getNegativeCount() {
+                return getRatingCount(FlashcardTest.Rating.NEGATIVE);
+            }
+
+            @Override
+            public double getNegativePercent() {
+                return getPercentage(getNegativeCount());
+            }
+
+            @Override
+            public int getNeutralCount() {
+                return getRatingCount(FlashcardTest.Rating.NEUTRAL);
+            }
+
+            @Override
+            public double getNeutralPercent() {
+                return getPercentage(getNeutralCount());
+            }
+
+            @Override
+            public int getPositiveCount() {
+                return getRatingCount(FlashcardTest.Rating.POSITIVE);
+            }
+
+            @Override
+            public double getPositivePercent() {
+                return getPercentage(getPositiveCount());
+            }
+
+            @Override
+            public int getRatedTotal() {
+                return getTotal() - getSkipCount();
+            }
+
+            @Override
+            public int getSkipCount() {
+                return initialCount - flashcardTestMap.size();
+            }
+
+            @Override
+            public double getSkipPercent() {
+                return getPercentage(getSkipCount());
+            }
+
+            @Override
+            public int getTotal() {
+                return initialCount;
+            }
+
+            private int getRatingCount(final FlashcardTest.Rating rating) {
+                return ListUtils.filter(new ArrayList<>(flashcardTestMap.values()), new
+                        Predicate<FlashcardTest>() {
+                            @Override
+                            public boolean apply(FlashcardTest flashcardTest) {
+                                return flashcardTest.getRating() == rating;
+                            }
+                        }).size();
+            }
+
+            private double getPercentage(int count) {
+                return (double) count / (double) getRatedTotal();
+            }
+        };
     }
 
     public static class EXTRA_KEYS {
