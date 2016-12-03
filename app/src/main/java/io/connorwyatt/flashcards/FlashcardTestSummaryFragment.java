7c707e31120bf3d1e422fd7edd73ba8f29e6b800
Connor Wyatt <connorwyatt1@gmail.com>
2016-12-03 17:03:03 +0000

    Added finish button to summary card

--- app/src/main/java/io/connorwyatt/flashcards/FlashcardTestSummaryFragment.java
+++ app/src/main/java/io/connorwyatt/flashcards/FlashcardTestSummaryFragment.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import io.connorwyatt.flashcards.activities.FlashcardTestActivity;
 
 public class FlashcardTestSummaryFragment extends Fragment {
     @Override
@@ -12,6 +15,18 @@ public class FlashcardTestSummaryFragment extends Fragment {
                              Bundle savedInstanceState) {
         ViewGroup viewGroup = (ViewGroup) inflater
                 .inflate(R.layout.fragment_flashcard_test_summary_card, container, false);
+
+        Button finishButton = (Button) viewGroup.findViewById(R.id
+                .flashcard_test_summary_finish_button);
+        finishButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FlashcardTestActivity testActivity = (FlashcardTestActivity)
+                        FlashcardTestSummaryFragment.this.getActivity();
+
+                testActivity.finish();
+            }
+        });
 
         return viewGroup;
     }
