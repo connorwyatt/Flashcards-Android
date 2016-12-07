df9680b26fe9fe50f1b04f3a1c8c3af34c7e418f
Connor Wyatt <connorwyatt1@gmail.com>
2016-12-07 19:22:30 +0000

    Added RecyclerView to categories activity

--- app/src/main/java/io/connorwyatt/flashcards/activities/CategoriesActivity.kt
+++ app/src/main/java/io/connorwyatt/flashcards/activities/CategoriesActivity.kt
@@ -4,17 +4,28 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.support.v7.widget.Toolbar
 import io.connorwyatt.flashcards.R
+import io.connorwyatt.flashcards.adapters.CategoryListAdapter
+import io.connorwyatt.flashcards.data.entities.Category
+import io.connorwyatt.flashcards.services.CategoryService
 
 class CategoriesActivity : AppCompatActivity()
 {
+    private var categories: List<Category>? = null
+
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_categories)
 
+        categories = CategoryService(this).getAll()
+
         setUpToolbar()
+
+        setUpRecycler(categories as List<Category>)
     }
 
     private fun setUpToolbar()
@@ -22,6 +33,15 @@ class CategoriesActivity : AppCompatActivity()
         val toolbar = findViewById(R.id.categories_toolbar) as Toolbar
         toolbar.setTitle(R.string.categories_title)
         setSupportActionBar(toolbar)
+    }
+
+    private fun setUpRecycler(categories: List<Category>)
+    {
+        val categoryListAdapter = CategoryListAdapter(categories)
+
+        val recycler = findViewById(R.id.categories_recycler) as RecyclerView
+        recycler.adapter = categoryListAdapter
+        recycler.layoutManager = LinearLayoutManager(this)
     }
 
     companion object Activities
