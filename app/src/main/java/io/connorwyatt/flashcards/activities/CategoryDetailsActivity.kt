d580eb04f2413afe93b0cde30e120c3edd44d5e0
Connor Wyatt <connorwyatt1@gmail.com>
2016-12-11 20:09:50 +0000

    Fixed issue where creating a new category would cause a validation error in the name field of the details form

--- app/src/main/java/io/connorwyatt/flashcards/activities/CategoryDetailsActivity.kt
+++ app/src/main/java/io/connorwyatt/flashcards/activities/CategoryDetailsActivity.kt
@@ -31,10 +31,6 @@ class CategoryDetailsActivity : AppCompatActivity()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_category_details)
 
-        if (intent.hasExtra(CATEGORY_ID))
-        {
-            category = categoryService.getById(intent.getLongExtra(CATEGORY_ID, -1))
-        }
 
         name = findViewById(R.id.category_details_name) as TextInputEditText
         nameLayout = findViewById(R.id.category_details_name_layout) as TextInputLayout
@@ -45,7 +41,12 @@ class CategoryDetailsActivity : AppCompatActivity()
 
         saveButton!!.setOnClickListener { save() }
 
-        updateViewFromCategory(category)
+        if (intent.hasExtra(CATEGORY_ID))
+        {
+            category = categoryService.getById(intent.getLongExtra(CATEGORY_ID, -1))
+            updateViewFromCategory(category)
+        }
+
         updateButton()
 
         val toolbar = findViewById(R.id.category_details_toolbar) as Toolbar
@@ -215,7 +216,8 @@ class CategoryDetailsActivity : AppCompatActivity()
         {
             val intent = Intent(context, CategoryDetailsActivity::class.java)
 
-            if (category !== null) {
+            if (category !== null)
+            {
                 intent.putExtra(CATEGORY_ID, category.id)
             }
 
