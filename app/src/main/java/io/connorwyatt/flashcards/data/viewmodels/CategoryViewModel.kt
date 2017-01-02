27ad0b2cc935cc8fdbaed31ef7877b6789f28038
Connor Wyatt <connorwyatt1@gmail.com>
2017-01-02 09:17:06 +0000

    Added CategoryViewModel with factory method

---
+++ app/src/main/java/io/connorwyatt/flashcards/data/viewmodels/CategoryViewModel.kt
@@ -0,0 +1,34 @@
+package io.connorwyatt.flashcards.data.viewmodels
+
+import io.connorwyatt.flashcards.data.entities.Category
+import io.connorwyatt.flashcards.data.services.FlashcardTestService
+import io.connorwyatt.flashcards.enums.Rating
+import io.reactivex.Observable
+
+data class CategoryViewModel(var category: Category, var flashcardCount: Int, var rating: Rating?)
+{
+    companion object
+    {
+        fun getFromCategory(category: Category,
+                            includeRating: Boolean = true): Observable<CategoryViewModel>
+        {
+            val ratingObservable: Observable<*>
+
+            if (includeRating)
+            {
+                ratingObservable = FlashcardTestService.getAverageRatingForCategory(category.id!!)
+            }
+            else
+            {
+                ratingObservable = Observable.just(false)
+            }
+
+            return ratingObservable.map {
+                val flashcardCount = category.relationships.getRelationships("flashcard")?.count() ?: 0
+                val rating = if (it is Double) Rating.fromValue(it) else null
+
+                CategoryViewModel(category, flashcardCount, rating)
+            }
+        }
+    }
+}
