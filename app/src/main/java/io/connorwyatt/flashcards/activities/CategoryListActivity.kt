51211abdd0e4b954b1a51527b155a76ef7998893
Connor Wyatt <connorwyatt1@gmail.com>
2017-01-02 10:20:28 +0000

    Added data to CategoryListActivity

--- app/src/main/java/io/connorwyatt/flashcards/activities/CategoryListActivity.kt
+++ app/src/main/java/io/connorwyatt/flashcards/activities/CategoryListActivity.kt
@@ -3,15 +3,79 @@ package io.connorwyatt.flashcards.activities
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import io.connorwyatt.flashcards.R
+import io.connorwyatt.flashcards.adapters.CategoryListAdapter
+import io.connorwyatt.flashcards.data.services.CategoryService
+import io.connorwyatt.flashcards.data.viewmodels.CategoryViewModel
+import io.reactivex.Observable
 
 class CategoryListActivity : BaseActivity()
 {
+    private val categoryListAdapter = CategoryListAdapter()
+
+    //region Activity
+
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_category_list)
+
+        initialiseUI()
     }
+
+    //endregion
+
+    //region Data
+
+    private fun getData(): Observable<List<CategoryViewModel>>
+    {
+        return CategoryService.getAll().flatMap { categories ->
+            val observables = categories.map {
+                CategoryViewModel.getFromCategory(it)
+            }
+
+            if (observables.isNotEmpty())
+            {
+                Observable.combineLatest(
+                    observables,
+                    { it.filterIsInstance(CategoryViewModel::class.java) }
+                )
+            }
+            else
+            {
+                Observable.just(listOf())
+            }
+        }
+    }
+
+    //endregion
+
+    //region UI
+
+    private fun initialiseUI(): Unit
+    {
+        initialiseRecycler()
+    }
+
+    private fun initialiseRecycler(): Unit
+    {
+        getData().subscribe {
+            updateAdapterData(it)
+        }
+
+        val recycler = findViewById(R.id.category_list_recycler) as RecyclerView
+        recycler.adapter = categoryListAdapter
+        recycler.layoutManager = LinearLayoutManager(this)
+    }
+
+    private fun updateAdapterData(viewModels: List<CategoryViewModel>): Unit
+    {
+        categoryListAdapter.setItems(viewModels)
+    }
+
+    //endregion
 
     companion object
     {
