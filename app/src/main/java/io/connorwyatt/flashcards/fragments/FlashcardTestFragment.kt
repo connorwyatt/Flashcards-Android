a9fe2c03fb101aaac5fba30a8d3e502d5e53de0b
Connor Wyatt <connorwyatt1@gmail.com>
2017-01-05 23:44:02 +0000

    Added fragment layout

--- app/src/main/java/io/connorwyatt/flashcards/fragments/FlashcardTestFragment.kt
+++ app/src/main/java/io/connorwyatt/flashcards/fragments/FlashcardTestFragment.kt
@@ -1,11 +1,32 @@
 package io.connorwyatt.flashcards.fragments
 
 import android.app.Fragment
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import io.connorwyatt.flashcards.R
 
 class FlashcardTestFragment : Fragment()
 {
+    //region Activity
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup,
+                              savedInstanceState: Bundle?): View
+    {
+        super.onCreateView(inflater, container, savedInstanceState)
+
+
+        val viewGroup = inflater.inflate(
+            R.layout.fragment_flashcard_test, container, false) as ViewGroup
+
+        return viewGroup
+    }
+
     fun onBackPressed(callback: () -> Unit): Unit
     {
         callback.invoke()
     }
+
+    //endregion
 }
