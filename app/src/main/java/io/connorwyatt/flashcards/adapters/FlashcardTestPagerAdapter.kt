1326f8a40bad728920c113db5c57081e51f52662
Connor Wyatt <connorwyatt1@gmail.com>
2017-01-06 22:45:14 +0000

    Added FlashcardTestPagerAdapter

---
+++ app/src/main/java/io/connorwyatt/flashcards/adapters/FlashcardTestPagerAdapter.kt
@@ -0,0 +1,67 @@
+package io.connorwyatt.flashcards.adapters
+
+import android.app.Fragment
+import android.app.FragmentManager
+import android.support.v4.view.PagerAdapter
+import io.connorwyatt.flashcards.data.entities.Flashcard
+
+class FlashcardTestPagerAdapter(fragmentManager: FragmentManager)
+    : FixedFragmentStatePagerAdapter(fragmentManager)
+{
+    private val summaryFragment = Fragment()
+    private var flashcards: MutableList<Flashcard>? = null
+
+    //region Pager
+
+    override fun getItem(position: Int): Fragment
+    {
+        val isFlashcard = position < flashcards?.size ?: 0
+
+        return if (isFlashcard) getFlashcardFragment(position) else summaryFragment
+    }
+
+    override fun getItemPosition(item: Any?): Int
+    {
+        return PagerAdapter.POSITION_NONE
+    }
+
+    override fun getFragmentTag(position: Int): String
+    {
+        return if (position < count) flashcards?.get(position)?.id!! else "summary"
+    }
+
+    override fun getCount(): Int
+    {
+        return flashcards?.size ?: 0
+    }
+
+    //endregion
+
+    //region Data
+
+    fun setData(flashcards: List<Flashcard>): Unit
+    {
+        this.flashcards = flashcards.toMutableList()
+        notifyDataSetChanged()
+    }
+
+    fun removeItem(position: Int): Flashcard
+    {
+        val removedFlashcard = flashcards!!.removeAt(position)
+        notifyDataSetChanged()
+        return removedFlashcard
+    }
+
+    //endregion
+
+    //region Fragments
+
+    private fun getFlashcardFragment(position: Int): Fragment
+    {
+        val fragment = Fragment()
+
+        return fragment
+    }
+
+    //endregion
+}
