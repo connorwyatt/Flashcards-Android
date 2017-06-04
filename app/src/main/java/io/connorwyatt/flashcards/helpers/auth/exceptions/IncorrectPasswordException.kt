29a2efc6fe92346cb93ae34cfa5751ed4d021a15
Connor Wyatt <connorwyatt1@gmail.com>
2017-06-04 01:10:40 +0100

    add email validation and error handling for AuthActivity

--- app/src/main/java/io/connorwyatt/flashcards/helpers/auth/exceptions/IncorrectPasswordException.kt
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright (c) 2016-2017 Connor Wyatt <connorwyatt1@gmail.com>.
- *
- * This file can not be copied and/or distributed without the express permission of Connor Wyatt.
- */
-
-package io.connorwyatt.flashcards.helpers.auth.exceptions
-
-class IncorrectPasswordException : AuthException()
